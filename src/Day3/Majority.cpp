034f2e89e529f61300ace9bb481b57d05e0eb1b9
DeboDevelop <debajyotidofficial@gmail.com>
2021-05-02 15:07:48 +0530

    Added Solution for Majority Element.

---
+++ src/Day3/Majority.cpp
@@ -0,0 +1,32 @@
+/**
+
+Majority Element
+
+Link: https://leetcode.com/problems/majority-element/
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int count = 0, candidate = 0;
+
+    for(int num : nums) {
+        if(count == 0) {
+            candidate = num;
+        }
+        if(num == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+}
+
+int main() {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    cout << "Result: " << majorityElement(nums) << "\n";
+    return 0;
+}
